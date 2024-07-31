c0716ec298cb818cb257108e74ea72270d0de5ab
Ilyas Bintang Prayogi <ilyas.prayogi@gmail.com>
2024-07-31 10:49:24 +0700

    🏗️ Init project

---
+++ Lolibaba/Lolibaba/LolibabaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LolibabaApp.swift
+//  Lolibaba
+//
+//  Created by Ilyas Bintang Prayogi on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LolibabaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
